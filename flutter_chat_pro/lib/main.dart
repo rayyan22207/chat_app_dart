ef6cf7d4cf4c01e84c66ed1a76975fab231395f5
rayyan22207 <rayyan22207@gmail.com>
2024-07-02 01:18:06 +0500

    working on

--- flutter_chat_pro/lib/main.dart
+++ flutter_chat_pro/lib/main.dart
@@ -1,23 +1,39 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_pro/main_screens/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(savedThemeMode: savedThemeMode));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.savedThemeMode});
+  final AdaptiveThemeMode? savedThemeMode;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Chat',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return AdaptiveTheme(
+      light: ThemeData(
         useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.deepPurple,
       ),
-      home: HomeScreen(),
+      dark: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.purple,
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Chat',
+        theme: theme,
+        darkTheme: darkTheme,
+        home: const HomeScreen(),
+      ),
     );
   }
 }
--- flutter_chat_pro/lib/main_screens/home_screen.dart
+++ flutter_chat_pro/lib/main_screens/home_screen.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -8,10 +12,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
+    return const Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Home'),
+      // ),
       body: Center(
         child: Text('Welcome to the Home Screen'),
       ),
